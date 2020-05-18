a569dce5032e811c3b98983f2b7295d624bfabc0
Catalina Onu <catalinaonu@gmail.com>
2020-05-18 09:17:11 +0200

    change order of logo img

--- components/Data/infoCompanies.js
+++ components/Data/infoCompanies.js
@@ -25,15 +25,15 @@ const companies = {
         "logo": <img src='/primecrowd.png'/>,
         "description": "primeCROWD vernetzt wachstumsstarke Startups mit Investoren und ist eines der größten Startup-Investoren-Netzwerke Österreichs."
     },
-    "Delta": {
-        "id": "DELTA",
-        "logo": <img src='/delta.png'/>,
-        "description": "DELTA zählt zu den etabliertesten Gesamtdienstleistern für Immobilien, darunter die WU Wien, Tabakfabrik Linz und voestalpine."
-    },
     "Blockpit": {
         "id": "blockPit",
         "logo": <img src='/blockpit.svg'/>,
         "description": "Blockpiimt ermöglicht Steuerreports sowie Portfolio Management für Kryptowährungen und bildet die Schnittstelle für alle Beteiligten."
+    },
+    "Delta": {
+        "id": "DELTA",
+        "logo": <img src='/delta.png'/>,
+        "description": "DELTA zählt zu den etabliertesten Gesamtdienstleistern für Immobilien, darunter die WU Wien, Tabakfabrik Linz und voestalpine."
     },
     "Carployee": {
         "id": "Carployee",
